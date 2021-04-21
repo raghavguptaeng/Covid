08326a6a2d5bbccc48b89e57ae2706e51c8d4c06
raghav gupta <raghavguptaeng@users.noreply.github.com>
2021-04-21 17:42:32 +0530

    UI made more appealing by addition of bottom navigation bar

--- lib/Screens/WelcomeScreen.dart
+++ lib/Screens/WelcomeScreen.dart
@@ -1,5 +1,7 @@
 import 'package:covid/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   static String homeScreen = '/welcome';
@@ -8,17 +10,49 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _StartScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ScreenBackgroundcolor,
-      appBar: AppBar(
-        title: Text(
-          'HOME',
-          style: HeadingFontStyle,
-        ),
-        backgroundColor: ScreenBackgroundcolor,
-        elevation: 1,
+      appBar:kAppbar,
+
+      bottomNavigationBar:Padding(
+        padding: EdgeInsets.all(20.0),
+        child: GNav(
+            rippleColor: Colors.grey[300],
+            hoverColor: Colors.grey[100],
+            gap: 8,
+            activeColor: ImpColor,
+            color: Colors.white54,
+            iconSize: 24,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            duration: Duration(milliseconds: 400),
+            tabBackgroundColor: Colors.black,
+            tabs: [
+              GButton(
+                icon: FontAwesomeIcons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: FontAwesomeIcons.cartPlus,
+                text: 'Cart',
+              ),
+              GButton(
+                icon: FontAwesomeIcons.bookMedical,
+                text: 'Doctors',
+              ),
+              GButton(
+                icon: FontAwesomeIcons.user,
+                text: 'Profile',
+              ),
+            ],
+            selectedIndex: _selectedIndex,
+            onTabChange: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            }),
       ),
     );
   }
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 var ScreenBackgroundcolor = Color(0xFF303136);
 var ImpColor = Color(0xFFFBD35A);
 var HeadingFontStyle = GoogleFonts.workSans(
   fontSize: 40,
   fontWeight:FontWeight.w400,
+);
+var kAppbar = AppBar(
+  title: Text(
+    'HOME',
+    style: HeadingFontStyle,
+  ),
+  backgroundColor: ScreenBackgroundcolor,
+  elevation: 1,
 );
